f1c058643388eef72f97d8b9b9e898d8deb96ac1
Abin Joseph <itsmeabjs@gmail.com>
2021-04-23 16:37:28 +0530

    Start Up

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+
+};
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+const db = app.firestore();
+const auth = app.auth();
+const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+export { db, auth, GoogleAuthProvider, timestamp };
---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        API_KEY: process.env.API_KEY,
+        AUTH_DOMAIN:process.env.AUTH_DOMAIN,
+        NEXT_PUBLIC_PROJECT_ID:process.env.NEXT_PUBLIC_PROJECT_ID,
+        STORAGE_BUCKET:process.env.STORAGE_BUCKET,
+        MESSAGING_SENDER_ID:process.env.MESSAGING_SENDER_ID,
+        APP_ID:process.env.APP_ID,
+        MEASUREMENT_ID:process.env.MEASUREMENT_ID,
+    },
+  }
